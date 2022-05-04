bde97fc3d716e2b4e7622203e03771a862e1aeeb
EichiMirai <magavolf31@gmail.com>
2022-05-04 13:42:43 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" >
+  <title>LEARN ENGLISH</title>
+        <link href="css/Sasuke.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+            <link rel="stylesheet" href="css/footer.css">
+</head>
+      <body>
+
+        <header>
+
+          <a href="#" class="logo"> <img src="img/logo.png" alt=""</a>
+            <div class="right_header">
+
+
+              <ul class="mnu_top">
+
+              <li><a href="#">Home</a></li>
+              <li><a href="lessons.html">Lessons</a></li>
+
+              </ul>
+                <div class="btns">
+
+                    <a href="signIn.html" class="btn_light">Sign in</a>
+                      <a href="register.html" class="btn_green">Rigister</a>
+
+                    </div>
+          </div>
+        </header>
+        <section id="main_banner" style="background-image: url('img/hand.png');">
+          <div class="container_custom">
+            <div class="row">
+
+            </div>
+            <div class="inner_banner">
+              <h1>Study with us</h1>
+              <a href="#" class="btn btn_pink">Join The Movement</a>
+            </div>
+          </div>
+        </section>
+
+
+        <section id="lesson">
+
+          <div class="container_1400">
+            <h2>Steps of lessons</h2>
+            <div class="description">Lorem ipsum dolor sit amet.</div>
+             <div class="row_flex">
+              <div class="why_item">
+                <img src="img/book.png" alt="" class="img-responsive">
+                <div class="name">Lesson 1</div>
+                <div class="descr">Lorem ipsum dolor sit amet</div>
+             </div>
+             <div class="why_item">
+               <img src="img/book.png" alt="" class="img-responsive">
+               <div class="name">Lesson 2</div>
+               <div class="descr">Lorem ipsum dolor sit amet</div>
+            </div>
+            <div class="why_item">
+              <img src="img/book.png" alt="" class="img-responsive">
+              <div class="name">Lesson 3</div>
+              <div class="descr">Lorem ipsum dolor sit amet</div>
+           </div>
+            </div>
+          </div>
+
+
+
+
+
+
+        </section>
+        <footer class="footerf">
+          <div class="containerf">
+              <div class="rowf">
+                <div class="footer-colf">
+                  <h4>Home</h4>
+                  <ul>
+                    <li><a href="#">Lessons</a></li>
+
+                  </ul>
+                </div>
+                <div class="footer-colf">
+                  <h4>Lessons</h4>
+                  <ul>
+                    <li><a href="#">Levels</a></li>
+
+                  </ul>
+                </div>
+                <div class="footer-colf">
+                  <h4>Follow us</h4>
+                  <div class="social-linksf">
+                    <a href="https://www.instagram.com/sansyzbaev_m_/"><i class="fab fa-instagram"></i></a>
+
+                  </div>
+                </div>
+
+              </div>
+          </div>
+
+
+
+        </footer>
+
+      </body>
+</html>
